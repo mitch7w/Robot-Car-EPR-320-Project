305e7b3b6ea8e77e3ed81c94f681c3d540aba07b
Mitch <mitch7w@gmail.com>
2020-10-18 11:41:56 +0200

    SC3 communication works and simplifies program flow

--- src/main.cpp
+++ src/main.cpp
@@ -4,70 +4,26 @@
 #include <Wire.h>
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
-int numberCapTouches = 0;
-String currentState = "IDLE"; // have to have currentState and numberCapTouches as SOS is state and not dependent on numberCapTouches
-int newByte = 0;              // Receving byte for serial
+String currentState = "IDLE"; 
+int receivedInput[] = {-1, -1, -1, -1}; // Receving byte for serial
+int offsetValue = -1;
+int speedValue = -1;
+int pMax = 0;
+int deviationLeftOrRight = -1;
+int batteryLevel = -1 ;
+int DAT1Byte = -1 ;
+int DAT0Byte = -1 ;
+int DECByte = -1 ;
+int SYSByte[] = {-1, -1} ;
+int SUBByte[] = {-1, -1} ;
+int ISTByte[] = {-1, -1, -1, -1} ;
+String lineColour = "GREEN" ;
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  Serial.begin(9600);
+  Serial.begin(19200);
   u8g2.begin();
-}
-
-void capacitiveTouch()
-{
-  // Detect capacitive touch press on D4 and increment numberCapTouches and change states
-  numberCapTouches++;
-  if (numberCapTouches == 3)
-  {
-    numberCapTouches = 0;
-  }
-  // Now decide which state to run
-  if (numberCapTouches == 0)
-  {
-    currentState = "IDLE";
-  }
-  if (numberCapTouches == 1)
-  {
-    currentState = "CAL";
-  }
-  if (numberCapTouches == 2)
-  {
-    currentState = "RACE";
-  }
-}
-
-void idleState()
-{
-}
-
-void calState()
-{
-}
-
-void raceState()
-{
-}
-
-void sosState()
-{
-}
-
-void endOfLineState()
-{
-}
-
-void audioDetection()
-{
-  // if SOS audio signal detected
-  currentState = "SOS";
-}
-
-void steeringControl()
-{
-  // implement speed and steering instructions based on motor speed info received
+  delay(1000) ;
 }
 
 void writeToScreen()
@@ -75,41 +31,126 @@ void writeToScreen()
   // Output line offset, color detected at central sensor, system state, motor speed, powerpack level to OLED screen
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_courB08_tf);
-  String offsetText = "Offset: " + String(touchRead(4)) + " mm";
+  String offsetText = "Offset: " + String(offsetValue) + " mm";
   u8g2.drawStr(0, 8, offsetText.c_str());
   u8g2.drawStr(0, 16, "Colour: Red");
-  String speedText = "Speed: " + String(touchRead(4)) + " rpm";
+  String speedText = "Speed: " + String(speedValue) + " rpm";
   u8g2.drawStr(0, 24, speedText.c_str());
-  String stateText = "State: " + currentState;
+  String stateText = "State: " + raceState;
   u8g2.drawStr(0, 32, stateText.c_str());
   u8g2.setFont(u8g2_font_unifont_t_77);
   u8g2.drawGlyph(105, 8, 0x26A1);
   u8g2.setFont(u8g2_font_courB08_tf);
-  String battText = String(touchRead(4)) + "%";
+  String battText = String(batteryLevel) + "%";
   u8g2.drawStr(100, 24, battText.c_str());
   u8g2.sendBuffer();
-  delay(500);
 }
 
-void communicateToHub(String message)
+void idleState()
 {
-  // Send correct bits over serial port using Serial.write(byte)
-  // Packet must be SYS<1:0> SUB<1:0> IST<3:0> DAT1<7:0> DAT0<7:0> DEC<7:0>
-  // System state, subsystem number Subsystem state, upper data byte, lower data byte, decimal data byte
-  // set individual states then append and convert to int at end
-  // 0 0 0 Hub initiate system and move to IDLE state
-  // 0 0 1 Hub terminate system and move to end of line state
-  // 0 1 0 Button press detected in IDLE state or not determines DAT1 contents
-  // 1 1 0 Button press detected in CAL state or not determines DAT1 contents
-  // 2 1 1 Sound detection in RACE state DAT1 bytes contain whether emergency audio tone detected (SC3 moves to motor control if dat1 =0)
-  // 2 1 2 Speed control and Dat1 = right motor and dat0 = left motor speed in rpm
-  // 3 1 0 Button press detected - DAT 1 = button press or not. Dat0 = ascii for line colour. move to race state if dat1 = 1
+  if (touchRead(4) < 20) // check for capacitive touch to move to next state
+  {
+    currentState = "CAL";
+  }
+  if (touchRead(2) < 20) // check for capacitive touch to change line colour
+  {
+    String tempColour ;
+    if(lineColour == "GREEN") {
+      tempColour = "RED" ;
+    }
+    if(lineColour == "RED") {
+      tempColour = "BLUE" ;
+    }
+    if (lineColour == "BLUE") {
+      tempColour = "EXTREME";
+    }
+    if(lineColour == "EXTREME") {
+      tempColour = "GREEN" ;
+    }
+    lineColour = tempColour ;
+  }
+  // Update hub with new info. SYS, SUB, IST, DAT1, DAT0, DEC
+  Serial.write(16) ; // 0b00010000
+  if(currentState == "CAL") { // button has been pressed
+    Serial.write(1) ; // DAT1 = 1
+  }
+  else {
+    Serial.write(0) ; // DAT1 = 0
+  }
+  Serial.write(lineColour[0]) ; // DAT0 = line colour
+  Serial.write(0) ; // DEC = 0
+}
+
+void calState()
+{
+  if (touchRead(4) < 20) // check for capacitive touch to move to next state // TODO make sure doesn't accidently trigger from previous press in previous state
+  {
+    currentState = "RACE";
+  }
+
+  // receive info from other systems, update variables and display critical info on screen
+
+  // Update hub with new info. SYS, SUB, IST, DAT1, DAT0, DEC
+  Serial.write(80); // 0b01010000
+  if (currentState == "RACE")
+  {                  // button has been pressed
+    Serial.write(1); // DAT1 = 1
+  }
+  else
+  {
+    Serial.write(0); // DAT1 = 0
+  }
+  Serial.write(lineColour[0]); // DAT0 = line colour
+  Serial.write(0);             // DEC = 0
+}
+
+void raceState()
+{
+if(soundDetected) {
+  currentState = "SOS" ;
+}
+
+  // Update hub with speed control
+  Serial.write(146); // 0b10010010
+  Serial.write() ; //DATA1 = right motor speed
+  Serial.write(); // DAT0 = left motor speed
+  Serial.write(0); // DEC = 0
+
+  if (currentState == "SOS") { // sound has been detected
+    // Update hub with sound detection
+    Serial.write(145); // 0b10010001
+    Serial.write(1);    //DATA1 = sound detected
+    Serial.write(0);    // DAT0 = 0 Don't care
+    Serial.write(0);   // DEC = 0 Don't care
+  }
+}
+
+void sosState()
+{
+  if (touchRead(4) < 20) // check for capacitive touch to move back to RACE state
+  {
+    currentState = "RACE";
+  }
+  // Update hub with button pressed status
+  Serial.write(208); // 0b11010000
+  if (currentState == "RACE") // button has been pressed
+  {
+    Serial.write(1); // DAT1 = 1
+  }
+  else
+  {
+    Serial.write(0); // DAT1 = 0
+  }
+  Serial.write(lineColour[0]); // DAT0 = line colour
+  Serial.write(0);             // DEC = 0
+}
+
+void endOfLineState()
+{
 }
 
 void loop()
 {
-  writeToScreen();
-  // main loop runs correct state as well as listening for SOS tone.
   // Basically a state switch block.
   if (currentState == "IDLE")
   {
@@ -127,44 +168,23 @@ void loop()
   {
     sosState();
   }
+}
 
-  // Now check capacitive touch?
-  if (touchRead(4) < 20)
-  {
-    capacitiveTouch();
+void receiveInput() {
+  if (Serial.available() >= 4)
+  { // There is data to read
+    int tempFirstByte = Serial.read();
+    SYSByte[0] = bitRead(tempFirstByte, 7);
+    SYSByte[1] = bitRead(tempFirstByte, 6);
+    SUBByte[0] = bitRead(tempFirstByte, 5);
+    SUBByte[1] = bitRead(tempFirstByte, 4);
+    ISTByte[0] = bitRead(tempFirstByte, 3);
+    ISTByte[1] = bitRead(tempFirstByte, 2);
+    ISTByte[2] = bitRead(tempFirstByte, 1);
+    ISTByte[3] = bitRead(tempFirstByte, 0);
+    DAT1Byte = Serial.read();
+    DAT0Byte = Serial.read();
+    DECByte = Serial.read();
+    // variables now have new input values in them
   }
-  // Reads here are SC3 system receiving info from HUB
-  // Writes here are SC3 system sending info to HUB
-  // // read the incoming byte:
-  // String incomingByte = Serial.readString();
-  if (Serial.available() > 0)
-  { // THere is data to read
-    char data = Serial.read();
-    char str[2];
-    str[0] = data;
-    str[1] = '\0';
-    Serial.print(str);
-  }
-  Serial.println(analogRead(36));
 }
-
-void buttonPressedSerialOutput()
-{
-  // Output button pressed state
-  Serial.write(16);
-  //Serial.write(c) ; with c=1 button pressed & c=0 button not pressed
-  Serial.write(71);
-  Serial.write(0);
-}
-
-// only happens after buttonPressed=true and processed all input data
-void motorControlSerialOutput()
-{
-  // Output motor control
-  Serial.write(145);
-  // Serial.write(50% pmax);
-  // Serial.write(50 % pmax);
-  Serial.write(0);
-}
-
-// TODO: Logic of serial when what is pressed
